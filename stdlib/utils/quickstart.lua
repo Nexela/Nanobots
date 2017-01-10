b3cdf58fac2916e4713aba4a3017876668f4a85c
Nexela <nexela@gmail.com>
2017-01-10 10:50:32 -0500

    quickstart re-write, now more mod friendly

--- stdlib/utils/quickstart.lua
+++ stdlib/utils/quickstart.lua
@@ -1,60 +1,67 @@
 require("stdlib.event.event")
+require("stdlib.config.config")
+
+--local QS = QS or {} --luacheck: no global
+local QS = Config.new(QS or {}) --luacheck: no global
+
+if remote.interfaces["quickstart-script"] then
+  game.print("Existing quickstart script - "..remote.call("quickstart-script", "creative_mode_quickstart_registerd_to"))
+  return remote.call("quickstart-script", "creative_mode_quickstart_registerd_to")
+end
+remote.add_interface("quickstart-script", {creative_mode_quickstart_registerd_to = function () return QS.get("mod_name", "not-set") end})
 
 local Area=require("stdlib.area.area")
 local quickstart = {}
-
+--quickstart.map = require("test")
 function quickstart.on_player_created(event)
-  local player = game.players[event.player_index]
+  if #game.players == 1 then
+    local player = game.players[event.player_index]
 
-  local simple_stack =
-  {
-    "deconstruction-planner",
-    "creative-mode_fluid-source",
-    "creative-mode_energy-source",
-    "creative-mode_super-electric-pole",
-    "construction-robot",
-    "pipe",
-    "gasturbinemk01",
-    "distilator",
-    "blueprint",
-  }
+    if QS.get("clear_items", false) then
+      player.clear_items_inside()
+    end
 
-  for _, item in pairs(simple_stack) do
-    if game.item_prototypes[item] then
-      player.insert(item)
+    local simple_stacks = QS.get("stacks", {})
+
+    for _, item in pairs(simple_stacks) do
+      if game.item_prototypes[item] then
+        player.insert(item)
+      end
+    end
+
+    if QS.get("power_armor", true) then
+      --Put on power armor, install equipment
+      local inv = player.get_inventory(defines.inventory.player_armor)
+      inv.insert("power-armor-mk2")
+      local armor = inv[1].grid
+      armor.put{name="fusion-reactor-equipment"}
+      armor.put{name="personal-roboport-equipment"}
+    end
+
+    local surface = player.surface
+    local area = {{-250, -250}, {250, 250}}
+
+    if QS.get("destroy_everything", true) then
+      if remote.interfaces["RSO"] and remote.interfaces["RSO"]["disableChunkHandler"] then
+        remote.call("RSO", "disableChunkHandler")
+      end
+      for _, entity in pairs(surface.find_entities(area)) do
+        if entity.name ~= "player" then
+          entity.destroy()
+        end
+      end
+    end
+
+    if QS.get("floor_tile", false) then
+      local tiles = {}
+      for x, y in Area.spiral_iterate(area) do
+        tiles[#tiles+1]={name=QS.get("floor_tile", "concrete"), position={x=x, y=y}}
+      end
+      surface.set_tiles(tiles, true)
+      --surface.set_tiles(quickstart.map, true)
     end
   end
-
-  --Put on power armor, install equipment
-  local inv = player.get_inventory(defines.inventory.player_armor)
-  inv.insert("power-armor-mk2")
-  local armor = inv[1].grid
-  armor.put{name="fusion-reactor-equipment"}
-  armor.put{name="personal-roboport-equipment"}
-
 end
 Event.register(defines.events.on_player_created, quickstart.on_player_created)
---quickstart.map = require("test")
-function quickstart.on_player_joined_game(event)
-  --local map = map
-  local player = game.players[event.player_index]
-  local surface = player.surface
-  local area = {{-250, -250}, {250, 250}}
-  local entities = surface.find_entities(area)
-  for _, entity in pairs(entities) do
-    if entity.name ~= "player" then
-      entity.destroy()
-    end
-  end
-
-  local tiles = {}
-  for x, y in Area.spiral_iterate(area) do
-    tiles[#tiles+1]={name="grass", position={x=x, y=y}}
-  end
-    surface.set_tiles(tiles, true)
-  --surface.set_tiles(quickstart.map, true)
-
-end
-Event.register(defines.events.on_player_joined_game, quickstart.on_player_joined_game)
 
 return quickstart
