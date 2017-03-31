5b561ae79bd3df6f8bf8ab20d8ef4d4255de1578
Nexela <nexela@gmail.com>
2017-03-31 11:51:57 -0400

    increment version

--- changes.lua
+++ changes.lua
@@ -10,7 +10,7 @@ old_version :: string: Old version of the mod. May be nil if the mod wasn't prev
 new_version :: string: New version of the mod. May be nil if the mod is no longer present (i.e. it was just removed).
 --]]
 local mod_name = MOD.name or "not-set"
-local migrations = {"1.2.0", "1.2.3", "1.6.0"}
+local migrations = {"1.2.0", "1.2.3", "1.6.1"}
 local changes = {}
 
 --Mark all migrations as complete during Init.
@@ -93,7 +93,7 @@ end
 local Queue = require("scripts/queue")
 local Player = require("scripts/player")
 local Force = require("scripts/force")
-changes["1.6.0"] = function ()
+changes["1.6.1"] = function ()
     global.forces = Force.init()
     global.players = Player.init()
     global.robointerfaces = {}
