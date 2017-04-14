40f6838bec8cff70423571adcbda6c1a3bd0924c
Nexela <nexela@gmail.com>
2017-04-14 05:32:47 -0400

    Cell Queue merges ticks among forces, closes #38

--- scripts/robointerface/robointerface.lua
+++ scripts/robointerface/robointerface.lua
@@ -146,7 +146,8 @@ local function run_interface(interface)
             local parameters = get_parameters(behaviour.parameters)
             -- If the closest roboport signal is present and > 0 then just run on the attached cell
             local just_cell = (parameters["nano-signal-closest-roboport"] or 0) > 0 and logistic_cell and {logistic_cell} or nil
-            local next_tick = Queue.next(queue, game.tick, tick_spacing, true)
+            local fdata = global.forces[logistic_cell.owner.force.name]
+            local next_tick = Queue.next(queue, fdata._next_cell_tick or game.tick, tick_spacing, true)
             for param_name, param_table in pairs(params_to_check) do
                 if (parameters[param_name] or 0) ~= 0 then
                     for _, cell in pairs(just_cell or logistic_network.cells) do
@@ -166,9 +167,9 @@ local function run_interface(interface)
                             Queue.insert(queue, data, next_tick())
                         end
                     end
-                    queue._next_tick = next_tick()
                 end
             end
+            fdata._next_cell_tick = next_tick()
         end
     end
 end
