27475462c4ee07dc67fe6df5e91adaeca8e1bf69
Nexela <nexela@gmail.com>
2018-05-11 21:03:39 -0400

    Fix typo in robot-brain-construction

--- data-updates.lua
+++ data-updates.lua
@@ -7,9 +7,9 @@ if mods['boblibrary'] then
     Recipe("ammo-nano-constructors"):replace_ingredient("electronic-circuit", "basic-circuit-board")
     Recipe("ammo-nano-termites"):replace_ingredient("electronic-circuit", "basic-circuit-board")
 
-    Recipe("equipment-bot-chip-items"):add_ingredient("robot-brain-constuction")
-    Recipe("equipment-bot-chip-trees"):add_ingredient("robot-brain-constuction")
-    Recipe("equipment-bot-chip-nanointerface"):add_ingredient("robot-brain-constuction")
+    Recipe("equipment-bot-chip-items"):add_ingredient("robot-brain-construction")
+    Recipe("equipment-bot-chip-trees"):add_ingredient("robot-brain-construction")
+    Recipe("equipment-bot-chip-nanointerface"):add_ingredient("robot-brain-construction")
     Recipe("equipment-bot-chip-nanointerface"):add_ingredient("gun-nano-emitter")
     Recipe("equipment-bot-chip-launcher"):add_ingredient("robot-brain-combat")
     Recipe("equipment-bot-chip-feeder"):add_ingredient("robot-brain-combat")
