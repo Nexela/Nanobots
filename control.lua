08f414732027fcb66035ee823973d0c9fb646dfc
Nicholas Dunn <nexela@gmail.com>
2019-05-25 18:01:48 -0400

    Changes Fix

--- control.lua
+++ control.lua
@@ -1,14 +1,13 @@
-local Event = require('__stdlib__/stdlib/event/event')
+local Event = require('__stdlib__/stdlib/event/event').set_protected_mode(true)
 local Interface = require('__stdlib__/stdlib/scripts/interface').merge_interfaces(require('interface'))
 local Commands = require('commands')
 
-Event.protected_mode = true
 Event.build_events = {defines.events.on_built_entity, defines.events.on_robot_built_entity}
 Event.mined_events = {defines.events.on_pre_player_mined_item, defines.events.on_robot_pre_mined}
 
 local Player = require('__stdlib__/stdlib/event/player').register_events(true)
 require('__stdlib__/stdlib/event/force').register_events(true)
-require('__stdlib__/stdlib/event/changes').register_events(true)
+require('__stdlib__/stdlib/event/changes').register_events('mod_versions', 'changes/versions')
 
 Player.additional_data({ranges = {}})
 
