ea6ac4775e622a659a547082dc68d99e2e9a57bc
Nexela <nexela@gmail.com>
2020-09-27 01:01:35 -0400

    Reorganize Settings

--- settings.lua
+++ settings.lua
@@ -4,39 +4,46 @@ data:extend{
         name = "nanobots-nanobots-auto",
         setting_type = "runtime-global",
         default_value = true,
-        order = "nanobots-aa[auto-bots-roll-out]-f"
+        order = "nanobots-aa[auto-bots-roll-out]"
+    },
+    {
+        type = "bool-setting",
+        name = "nanobots-equipment-auto",
+        setting_type = "runtime-global",
+        default_value = true,
+        order = "nanobots-ab[poll-rate]"
     },
     {
         type = "bool-setting",
         name = "nanobots-nano-build-tiles",
         setting_type = "runtime-global",
         default_value = true,
-        order = "nanobots-aa[build-tiles]-f"
+        order = "nanobots-ba[build-tiles]"
     },
     {
         type = "bool-setting",
         name = "nanobots-network-limits",
         setting_type = "runtime-global",
         default_value = true,
-        order = "nanobots-aa[check-networks]-f"
+        order = "nanobots-bb[check-networks]"
+    },
+    {
+        name = "nanobots-afk-time",
+        type = "int-setting",
+        setting_type = "runtime-global",
+        default_value = 4,
+        maximum_value = 60*60,
+        minimum_value = 0,
+        order = "nanobots-ca",
     },
     {
         type = "int-setting",
         name = "nanobots-nano-poll-rate",
         setting_type = "runtime-global",
         default_value = 60,
-        maximum_value = 60*60*60,
+        maximum_value = 60*60,
         minimum_value = 1,
-        order = "nanobots-ab[nano-poll-rate]-f"
-    },
-    {
-        type = "int-setting",
-        name = "nanobots-nano-queue-rate",
-        setting_type = "runtime-global",
-        default_value = 12,
-        maximum_value = 60*60*60,
-        minimum_value = 4,
-        order = "nanobots-ac[nano-queue-rate]-f"
+        order = "nanobots-ea[nano-poll-rate]"
     },
     {
         type = "int-setting",
@@ -45,16 +52,25 @@ data:extend{
         default_value = 100,
         maximum_value = 800,
         minimum_value = 1,
-        order = "nanobots-ad[nano-queue-rate]-f"
+        order = "nanobots-eb[nano-queue-rate]"
+    },
+    {
+        type = "int-setting",
+        name = "nanobots-nano-queue-rate",
+        setting_type = "runtime-global",
+        default_value = 12,
+        maximum_value = 60*60,
+        minimum_value = 4,
+        order = "nanobots-ec[nano-queue-rate]"
     },
     {
         type = "int-setting",
         name = "nanobots-cell-queue-rate",
         setting_type = "runtime-global",
         default_value = 5,
-        maximum_value = 60*60*60,
+        maximum_value = 60*60,
         minimum_value = 1,
-        order = "nanobots-ba[cell-queue-rate]-f"
+        order = "nanobots-fa[cell-queue-rate]"
     },
     {
         type = "int-setting",
@@ -63,22 +79,6 @@ data:extend{
         default_value = 50,
         maximum_value = 100,
         minimum_value = 1,
-        order = "nanobots-bb[free-bots-per]-f"
-    },
-    {
-        type = "bool-setting",
-        name = "nanobots-equipment-auto",
-        setting_type = "runtime-global",
-        default_value = true,
-        order = "nanobots-bc[poll-rate]-f"
-    },
-    {
-        name = "nanobots-afk-time",
-        type = "int-setting",
-        setting_type = "runtime-global",
-        default_value = 4,
-        order = "nanobots-bd",
-        maximum_value = 60*60,
-        minimum_value = 0,
+        order = "nanobots-fb[free-bots-per]"
     }
 }
